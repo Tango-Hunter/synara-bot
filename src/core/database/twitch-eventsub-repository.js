55216facbd31c102f0c4dbb935097e288fd810cb
Tango Hunter <hunting.tang0@gmail.com>
2026-07-22 17:43:56 -0400

    Update twitch-eventsub-repository.js

--- src/core/database/twitch-eventsub-repository.js
+++ src/core/database/twitch-eventsub-repository.js
@@ -2,7 +2,6 @@
  * Title: twitch-eventsub-repository.js
  * Author: Tango Hunter
  * Date Created: 5/29/26
- * Date Modified: 5/29/26
  * Description: EventSub .
  */
 
@@ -10,42 +9,78 @@ const pool
      = require('./postgres');
 
 
-async function getSubscription(
-    twitchUserId
-) {
+async function getSubscription({
+
+    twitchUserId,
+
+    subscriptionType
+
+}) {
 
     const result =
 
         await pool.query(
 
             `
+
             SELECT *
+
             FROM twitch_eventsub
-            WHERE twitch_user_id = $1
+
+            WHERE
+
+                twitch_user_id = $1
+
+            AND
+
+                subscription_type = $2
+
             `,
+
             [
-                twitchUserId
+
+                twitchUserId,
+
+                subscriptionType
+
             ]
+
         );
 
-    return result.rows[0] || null;
+    return (
+
+        result.rows[0]
+
+        ??
+
+        null
+
+    );
 }
 
 async function saveSubscription({
 
     twitchUserId,
 
+    subscriptionType,
+
     subscriptionId
+
 }) {
 
     await pool.query(
 
         `
+
         INSERT INTO twitch_eventsub (
 
             twitch_user_id,
 
-            subscription_id
+            subscription_type,
+
+            subscription_id,
+
+            last_verified_at
 
         )
 
@@ -53,31 +88,94 @@ async function saveSubscription({
 
             $1,
 
-            $2
+            $2,
+
+            $3,
+
+            NOW()
+
         )
 
         ON CONFLICT (
-            twitch_user_id
+
+            twitch_user_id,
+
+            subscription_type
+
         )
 
         DO UPDATE SET
 
             subscription_id =
+
                 EXCLUDED.subscription_id,
 
             updated_at =
+
+                NOW(),
+
+            last_verified_at =
+
                 NOW()
+
         `,
+
         [
 
             twitchUserId,
 
+            subscriptionType,
+
             subscriptionId
+
         ]
+
+    );
+}
+
+async function touchSubscription({
+
+    twitchUserId,
+
+    subscriptionType
+
+}) {
+
+    await pool.query(
+
+        `
+
+        UPDATE twitch_eventsub
+
+        SET
+
+            updated_at = NOW(),
+
+            last_verified_at = NOW()
+
+        WHERE
+
+            twitch_user_id = $1
+
+        AND
+
+            subscription_type = $2
+
+        `,
+
+        [
+
+            twitchUserId,
+
+            subscriptionType
+
+        ]
+
     );
 }
 
 module.exports = {
     getSubscription,
-    saveSubscription
+    saveSubscription,
+    touchSubscription
 };
